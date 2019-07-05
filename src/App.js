ba2603f6d9a846c510ed45cce86eb262673200fd
CristianoZitarosa <36627397+CristianoZitarosa@users.noreply.github.com>
2019-07-05 19:18:34 +0200

    iv

--- src/App.js
+++ src/App.js
@@ -14,15 +14,12 @@ class App extends Component {
 
           <hr />
 
-          <Route exact path="/" component={Home} />
-          <Route path="/about" component={About} />
+          <Route exact path="/" render={()=> <div><h2>Home</h2></div>} />
+          <Route path="/about" render={()=> <div><h2>About</h2></div>} />
         </div>
 
     );
   }
 }
-
-const Home = () => <div><h2>Home</h2></div>
-const About = () => <div><h2>About</h2></div>
 
 export default App;
