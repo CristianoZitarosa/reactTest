d716919ed038958fdae6c67469ea94185cfd30cf
CristianoZitarosa <36627397+CristianoZitarosa@users.noreply.github.com>
2019-07-05 18:45:07 +0200

    Updates

---
+++ precache-manifest.9efacc1eff358ceed96bd90b71b4c4cd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4ab4449c9f484a43e3da8c94fc4247a6",
+    "url": "/reactTest/index.html"
+  },
+  {
+    "revision": "e542b01d03fd8a82ea29",
+    "url": "/reactTest/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "f195c9a280a4db6fb6b0",
+    "url": "/reactTest/static/js/2.63cebfce.chunk.js"
+  },
+  {
+    "revision": "e542b01d03fd8a82ea29",
+    "url": "/reactTest/static/js/main.50b35a70.chunk.js"
+  },
+  {
+    "revision": "2ce7ab29404c74777852",
+    "url": "/reactTest/static/js/runtime~main.3be99dba.js"
+  }
+]);
